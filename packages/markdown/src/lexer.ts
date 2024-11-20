5be03ac337bc51208028e598447b45addb7fbfd8
sercrac07 <sercassan@gmail.com>
2024-11-20 12:52:08 +0100

    feat: refactor code

--- packages/markdown/src/lexer.ts
+++ packages/markdown/src/lexer.ts
@@ -1,271 +1,66 @@
-import { InlineToken, InlineTokenType, ListItemToken, Token, TokenType } from './types'
+import { Token, TokenType } from './types'
+
+class Lexer {
+  sources: string[]
+
+  constructor(source: string) {
+    this.sources = source.split(/\r?\n/g)
+  }
+
+  private current(): string {
+    return this.sources[0]
+  }
+  private eat(): string {
+    return this.sources.shift()!
+  }
+
+  public lexerize(): Token[] {
+    const tokens: Token[] = []
+
+    while (this.sources.length > 0) {
+      // TODO: revisar los `trim()`
+      if (this.current().trim() === '') tokens.push(this.lexerizeNewLine())
+      else if (/^\s*#{1,6}\s/.test(this.current())) tokens.push(this.lexerizeHeading())
+      else if (/^\s*```/.test(this.current())) tokens.push(this.lexerizeCodeBlock())
+      else if (/^\s*>/.test(this.current())) tokens.push(this.lexerizeBlockQuote())
+      else if (/^\s*-\s/.test(this.current()) || /^\s*\d+\.\s/.test(this.current())) tokens.push(this.lexerizeList())
+      else tokens.push(this.lexerizeParagraph())
+    }
+
+    tokens.push({ type: TokenType.EndOfFile })
+
+    return tokens
+  }
+
+  private lexerizeNewLine(): Token {
+    this.eat()
+    return { type: TokenType.NewLine }
+  }
+  private lexerizeParagraph(): Token {
+    return { type: TokenType.Paragraph, value: this.eat() }
+  }
+  private lexerizeHeading(): Token {
+    const level = this.current().match(/^\s*(#+)\s/)![1].length as 1 | 2 | 3 | 4 | 5 | 6
+    return { type: TokenType.Heading, level, value: this.eat() }
+  }
+  private lexerizeCodeBlock(): Token {
+    return { type: TokenType.CodeBlock, value: this.eat() }
+  }
+  private lexerizeBlockQuote(): Token {
+    return { type: TokenType.BlockQuote, value: this.eat() }
+  }
+  private lexerizeList(): Token {
+    const token = this.eat()
+    const ordered = /^\s*\d+\.\s/.test(token)
+    return { type: TokenType.List, value: token, ordered }
+  }
+}
 
 /**
- * This function takes a Markdown source string as input and returns an array of tokens.
- * It processes the input line by line, identifying various Markdown structures such as
- * headings, paragraphs, code blocks, block quotes, lists, and in-line elements (like
- * bold, italic, links, and images).
+ * Generates a list of tokens from a Markdown source.
+ *
+ * [API Reference](https://github.com/sercrac07/ast-exp/tree/master/packages/markdown#lexersource)
  */
-export default function lexerize(source: string): Token[] {
-  const tokens: Token[] = []
-  const lines = source.split(/\r?\n/g) // Split source into lines
-  const paragraphBuffer: string[] = [] // Buffer to collect paragraph content
-
-  // Process inline elements like text, emphasis, links, etc.
-  function handleInline(line: string): InlineToken[] {
-    const inlines: InlineToken[] = []
-    const chars = line.split('') // Convert line into array of characters
-    let text = ''
-
-    // Add text to inlines and reset buffer
-    function handleText() {
-      if (text.length === 0) return
-      inlines.push({ type: InlineTokenType.Text, value: text })
-      text = ''
-    }
-
-    // Loop through each character
-    while (chars.length > 0) {
-      const char = chars.shift()!
-
-      // Handle escaped characters
-      if (char === '\\') {
-        if (chars[0]) text += chars.shift()!
-      }
-      // Handle inline code
-      else if (char === '`') {
-        handleText()
-        let code = ''
-        while (chars.length > 0 && chars[0] !== '`') {
-          if (chars[0] === '\\') chars.shift()
-          code += chars.shift()!
-        }
-        chars.shift()
-        inlines.push({ type: InlineTokenType.Code, value: code })
-      }
-      // Handle strong emphasis (bold)
-      else if ((char === '*' || char === '_') && chars[0] === char) {
-        handleText()
-        chars.shift()
-        let strong = ''
-        while (chars.length > 0 && !(chars[0] === char && chars[1] === char)) {
-          if (chars[0] === ('\\' as any)) strong += chars.shift()
-          strong += chars.shift()!
-        }
-        chars.shift()
-        chars.shift()
-        inlines.push({ type: InlineTokenType.Strong, value: handleInline(strong) })
-      }
-      // Handle emphasis (italic)
-      else if (char === '*' || char === '_') {
-        handleText()
-        let emphasis = ''
-        while (chars.length > 0 && chars[0] !== char) {
-          if (chars[0] === '\\') emphasis += chars.shift()
-          emphasis += chars.shift()!
-        }
-        chars.shift()
-        inlines.push({ type: InlineTokenType.Emphasis, value: handleInline(emphasis) })
-      }
-      // Handle links and images
-      else if (char === '[') {
-        let value = ''
-        while (chars.length > 0 && chars[0] !== ']') {
-          if (chars[0] === '\\') chars.shift()
-          value += chars.shift()!
-        }
-        chars.shift()
-        if (chars[0] === '(') {
-          handleText()
-          chars.shift()
-          let url = ''
-          while (chars.length > 0 && chars[0] !== (')' as any)) {
-            if (chars[0] === ('\\' as any)) chars.shift()
-            url += chars.shift()!
-          }
-          chars.shift()
-          inlines.push({ type: InlineTokenType.Link, url, value: handleInline(value) })
-        } else text += `[${value}]`
-      }
-      // Handle images
-      else if (char === '!' && chars[0] === '[') {
-        chars.shift()
-        let value = ''
-        while (chars.length > 0 && chars[0] !== (']' as any)) {
-          if (chars[0] === ('\\' as any)) chars.shift()
-          value += chars.shift()!
-        }
-        chars.shift()
-        if (chars[0] === ('(' as any)) {
-          handleText()
-          chars.shift()
-          let url = ''
-          while (chars.length > 0 && chars[0] !== (')' as any)) {
-            if (chars[0] === ('\\' as any)) chars.shift()
-            url += chars.shift()!
-          }
-          chars.shift()
-          inlines.push({ type: InlineTokenType.Image, url, value })
-        } else text += `![${value}]`
-      }
-      // Handle HTML comments
-      else if (char === '<' && chars[0] === '!' && chars[1] === '-' && chars[2] === '-') {
-        handleText()
-        chars.shift()
-        chars.shift()
-        chars.shift()
-        chars.shift()
-        while (chars.length > 0 && !(chars[0] === ('-' as any) && chars[1] === '-' && chars[2] === ('>' as any))) {
-          if (chars[0] === ('\\' as any)) chars.shift()
-          chars.shift()!
-        }
-        chars.shift()
-        chars.shift()
-        chars.shift()
-      }
-      // Collect remaining text
-      else {
-        text += char
-      }
-    }
-
-    handleText()
-    return inlines
-  }
-
-  // Main loop for processing each line
-  while (lines.length > 0) {
-    const line = lines.shift()!
-
-    // Handle the buffered paragraph content
-    function handleBuffer() {
-      if (paragraphBuffer.length === 0) return
-      if (paragraphBuffer.join('') === '') return
-
-      tokens.push({
-        type: TokenType.Paragraph,
-        value: handleInline(paragraphBuffer.join('\n')),
-      })
-      paragraphBuffer.length = 0
-    }
-
-    // Detect headings
-    const isHeading = /^(#{1,6})\s/.exec(line)
-    if (isHeading) {
-      handleBuffer()
-
-      const headingLevel = isHeading[1].length as 1 | 2 | 3 | 4 | 5 | 6
-      const heading = line.replace(/^#+\s+/, '')
-
-      tokens.push({
-        type: TokenType.Heading,
-        level: headingLevel,
-        value: handleInline(heading),
-      })
-    }
-    // Handle code blocks
-    else if (line.startsWith('```')) {
-      handleBuffer()
-      const codeBlock: string[] = []
-      const match = line.match(/^```(\w+|\s)\s*(.*)/)
-      const language = match?.[1]
-      const meta = match?.[2]
-      while (lines.length > 0 && !lines[0].startsWith('```')) {
-        codeBlock.push(lines.shift()!)
-      }
-      lines.shift()
-      tokens.push({
-        type: TokenType.CodeBlock,
-        language: language === ' ' ? undefined : language,
-        meta: meta === '' ? undefined : meta,
-        value: codeBlock.join('\n'),
-      })
-    }
-    // Handle block quotes
-    else if (line.startsWith('> ')) {
-      handleBuffer()
-      const blockQuote: string[] = [line.replace(/^>\s+/, '')]
-      while (lines.length > 0 && lines[0].startsWith('> ')) {
-        blockQuote.push(lines.shift()!.replace(/^>\s+/, ''))
-      }
-      tokens.push({
-        type: TokenType.BlockQuote,
-        value: lexerize(blockQuote.join('\n')),
-      })
-    }
-    // Handle lists
-    else if (line.startsWith('- ') || /^\d+\.\s/.test(line)) {
-      handleBuffer()
-      const isOrdered = /^\d+\.\s/.test(line)
-      const cleanLine = isOrdered ? line.replace(/^\d+\.\s/, '') : line.replace(/^-\s+/, '')
-      const match = cleanLine.match(/^\[(.)\]\s(.+)/)
-      const checked = match?.[1] ?? null
-      const content = match?.[2] ?? cleanLine
-      const list: ListItemToken[] = [{ type: TokenType.ListItem, checked, value: lexerize(content) }]
-      const leveledList: string[] = []
-
-      while ((lines.length > 0 && (isOrdered ? /^\d+\.\s/.test(lines[0]) : /^-\s+/.test(lines[0]))) || /^\s{2,}/.test(lines[0])) {
-        if (/^\s{2,}/.test(lines[0])) leveledList.push(lines.shift()!.replace(/^\s{2}/, ''))
-        else {
-          if (leveledList.length > 0 && lexerize(leveledList.join('\n'))[0]) {
-            list[list.length - 1].value.push(lexerize(leveledList.join('\n'))[0])
-            leveledList.length = 0
-          }
-          const cleanLine = isOrdered ? lines.shift()!.replace(/^\d+\.\s/, '') : lines.shift()!.replace(/^-\s+/, '')
-          const match = cleanLine.match(/^\[(.)\]\s(.+)/)
-          const checked = match?.[1] ?? null
-          const content = match?.[2] ?? cleanLine
-          list.push({ type: TokenType.ListItem, checked, value: lexerize(content) })
-        }
-      }
-      if (leveledList.length > 0 && lexerize(leveledList.join('\n'))[0]) list[list.length - 1].value.push(lexerize(leveledList.join('\n'))[0])
-      tokens.push({
-        type: isOrdered ? TokenType.OrderedList : TokenType.UnorderedList,
-        value: list,
-      })
-    }
-    // Handle tables
-    else if (/^\|(.+)\|$/.test(line)) {
-      // Validate next line is a valid separator
-      if (/^\|(\s*[-:]{3,}\s*\|)+$/.test(lines[0])) {
-        const alignment = lines
-          .shift()!
-          .split('|')
-          .slice(1, -1)
-          .map(cell => {
-            const c = cell.trim()
-            if (c.startsWith(':')) {
-              return c.endsWith(':') ? 'center' : 'left'
-            } else if (c.endsWith(':')) {
-              return 'right'
-            }
-            return undefined
-          })
-
-        const header = line
-          .split('|')
-          .slice(1, -1)
-          .map(cell => handleInline(cell.trim()))
-
-        const rows: InlineToken[][][] = []
-        while (lines.length > 0 && lines[0].startsWith('|') && lines[0].endsWith('|')) {
-          const row = lines.shift()!.split('|').slice(1, -1)
-          rows.push(row.map(cell => handleInline(cell.trim())))
-        }
-
-        tokens.push({ type: TokenType.Table, header, rows, alignment })
-      } else {
-        paragraphBuffer.push(line)
-      }
-    }
-    // Handle paragraphs
-    else {
-      if (line.trim().length === 0) handleBuffer()
-      else paragraphBuffer.push(line)
-    }
-
-    if (lines.length === 0) handleBuffer()
-  }
-
-  return tokens
+export function lexer(source: string): Token[] {
+  return new Lexer(source).lexerize()
 }
--- packages/markdown/src/parser.ts
+++ packages/markdown/src/parser.ts
@@ -1,126 +1,97 @@
-import lexerize from './lexer'
-import { InlineNode, InlineNodeType, InlineToken, InlineTokenType, Node, NodeType, Token, TokenType } from './types'
+import { inline } from './inline'
+import { lexer } from './lexer'
+import { BlockQuoteToken, CodeBlockToken, HeadingToken, ListItemNode, ListToken, Node, NodeType, ParagraphToken, ProgramNode, Token, TokenType } from './types'
+
+class Parser {
+  private tokens: Token[]
+
+  constructor(source: string) {
+    this.tokens = lexer(source)
+  }
+
+  private current(): Token {
+    return this.tokens[0]
+  }
+  private eat(): Token {
+    return this.tokens.shift()!
+  }
+
+  public parse(): ProgramNode {
+    const program: ProgramNode = { type: NodeType.Program, children: [] }
+
+    while (this.current().type !== TokenType.EndOfFile) {
+      if (this.current().type === TokenType.Paragraph) program.children.push(this.parseParagraph())
+      else if (this.current().type === TokenType.Heading) program.children.push(this.parseHeading())
+      else if (this.current().type === TokenType.CodeBlock) program.children.push(this.parseCodeBlock())
+      else if (this.current().type === TokenType.BlockQuote) program.children.push(this.parseBlockQuote())
+      else if (this.current().type === TokenType.List) program.children.push(this.parseList())
+      else this.eat()
+    }
+
+    return program
+  }
+  private parseParagraph(): Node {
+    const paragraphs: string[] = []
+
+    while (this.current().type === TokenType.Paragraph) {
+      paragraphs.push((this.eat() as ParagraphToken).value)
+    }
+
+    return { type: NodeType.Paragraph, children: inline(paragraphs.join('\n')) }
+  }
+  private parseHeading(): Node {
+    const token = this.eat() as HeadingToken
+    return { type: NodeType.Heading, level: token.level, children: inline(token.value.replace(/^\s*#+\s*/, '')) }
+  }
+  private parseCodeBlock(): Node {
+    const token = this.eat() as CodeBlockToken
+    const language = token.value.match(/^\s*```(\w+)/)?.[1] || undefined
+    const meta = token.value.match(/^\s*```\w+\s+(.*)$/)?.[1] || undefined
+    const content: string[] = []
+    while (this.current().type !== TokenType.EndOfFile && this.current().type !== TokenType.CodeBlock) {
+      if ('value' in this.current()) content.push((this.eat() as any).value)
+      else {
+        this.eat()
+        content.push('')
+      }
+    }
+    if (this.current().type === TokenType.CodeBlock) this.eat()
+    return { type: NodeType.CodeBlock, language, meta, value: content.join('\n') }
+  }
+  private parseBlockQuote(): Node {
+    const blockQuotes: string[] = []
+
+    while (this.current().type === TokenType.BlockQuote) {
+      blockQuotes.push((this.eat() as BlockQuoteToken).value.replace(/^\s*>\s*/, ''))
+    }
+
+    return { type: NodeType.BlockQuote, children: parser(blockQuotes.join('\n')).children }
+  }
+  private parseList(): Node {
+    const listItems: string[] = []
+    const isOrdered = (this.current() as ListToken).ordered
+
+    while (this.current().type !== TokenType.EndOfFile) {
+      if (this.current().type === TokenType.List) listItems.push((this.eat() as ListToken).value.replace(/^\s*-\s|\s*\d+\.\s/, ''))
+      else if (!('value' in this.current())) {
+        this.eat()
+        listItems.push('')
+      } else if ((this.current() as any).value.startsWith('  ')) listItems.push((this.eat() as any).value.replace(/^\s{2}/, ''))
+      else break
+    }
+
+    const children: ListItemNode[] = parser(listItems.join('\n')).children.map(child => ({ type: NodeType.ListItem, children: [child] }))
+
+    if (isOrdered) return { type: NodeType.OrderedList, children }
+    return { type: NodeType.UnorderedList, children }
+  }
+}
 
 /**
- * Parses a Markdown string into an AST.
+ * Generates an AST from a Markdown source.
+ *
+ * [API Reference](https://github.com/sercrac07/ast-exp/tree/master/packages/markdown#parsersource)
  */
-export default function parser(source: string): Node {
-  const tokens = lexerize(source)
-
-  const program: Node = {
-    type: NodeType.Program,
-    children: [],
-  }
-
-  /**
-   * Converts an inline token to its corresponding Inline node representation.
-   */
-  function handleInline(inline: InlineToken): InlineNode {
-    switch (inline.type) {
-      case InlineTokenType.Text: {
-        return {
-          type: InlineNodeType.Text,
-          value: inline.value,
-        }
-      }
-      case InlineTokenType.Code: {
-        return {
-          type: InlineNodeType.Code,
-          value: inline.value,
-        }
-      }
-      case InlineTokenType.Strong: {
-        return {
-          type: InlineNodeType.Strong,
-          children: inline.value.map(handleInline),
-        }
-      }
-      case InlineTokenType.Emphasis: {
-        return {
-          type: InlineNodeType.Emphasis,
-          children: inline.value.map(handleInline),
-        }
-      }
-      case InlineTokenType.Link: {
-        return {
-          type: InlineNodeType.Link,
-          children: inline.value.map(handleInline),
-          url: inline.url,
-        }
-      }
-      case InlineTokenType.Image: {
-        return {
-          type: InlineNodeType.Image,
-          alt: inline.value,
-          url: inline.url,
-        }
-      }
-    }
-  }
-
-  /**
-   * Converts a block-level token to its corresponding Node representation.
-   */
-  function handleToken(token: Token): Node {
-    switch (token.type) {
-      case TokenType.Paragraph: {
-        return {
-          type: NodeType.Paragraph,
-          children: token.value.map(handleInline),
-        }
-      }
-      case TokenType.Heading: {
-        return {
-          type: NodeType.Heading,
-          children: token.value.map(handleInline),
-          level: token.level,
-        }
-      }
-      case TokenType.CodeBlock: {
-        return {
-          type: NodeType.CodeBlock,
-          value: token.value,
-          language: token.language,
-          meta: token.meta,
-        }
-      }
-      case TokenType.BlockQuote: {
-        return {
-          type: NodeType.BlockQuote,
-          children: token.value.map(handleToken),
-        }
-      }
-      case TokenType.OrderedList:
-      case TokenType.UnorderedList: {
-        return {
-          type: NodeType.List,
-          ordered: token.type === TokenType.OrderedList,
-          children: token.value.map(handleToken),
-        }
-      }
-      case TokenType.ListItem: {
-        return {
-          type: NodeType.ListItem,
-          checked: token.checked,
-          children: token.value.map(handleToken),
-        }
-      }
-      case TokenType.Table: {
-        return {
-          type: NodeType.Table,
-          header: token.header.map(cell => cell.map(handleInline)),
-          rows: token.rows.map(row => row.map(cell => cell.map(handleInline))),
-          alignment: token.alignment,
-        }
-      }
-    }
-  }
-
-  // Process tokens and build the AST.
-  while (tokens.length > 0) {
-    const token = tokens.shift()!
-    program.children.push(handleToken(token))
-  }
-
-  return program
+export function parser(source: string): ProgramNode {
+  return new Parser(source).parse()
 }
